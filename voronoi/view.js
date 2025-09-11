965716f7c78247e5c295ec607af391c3ca9c7e51
andyblaze <andy@mediagraphic.co.uk>
2025-09-11 15:30:48 +0100

    voronoi

--- voronoi/view.js
+++ voronoi/view.js
@@ -29,10 +29,14 @@ export default class View {
         this.offCtx.fillStyle = gradient;
         this.offCtx.fillRect(0, this.offscreen.height - height, this.offscreen.width, height);
     }
-    gradient(ctx, site, hue) {
-        const g = ctx.createRadialGradient(site.x, site.y, 0, site.x, site.y, 80); console.log(hue);
+    radialGradient(ctx, site, hue) { hue = parseInt(hue); if ( isNaN(hue) ) hue = 180;
+        const g = ctx.createRadialGradient(site.x, site.y, 0, site.x, site.y, 180);
         g.addColorStop(0, `hsla(${hue},80%,70%,0.7)`);
         g.addColorStop(1, `hsla(${hue},80%,30%,0.1)`);
+        ctx.fillStyle = g;
+    }
+    shimmer(t, i) {
+        return 0.3 + 0.2 * Math.sin(t * 0.002 + i);
     }
     draw(data) {
         this.offCtx.drawImage(this.skyImage, 0, 0, this.offscreen.width, this.offscreen.height);
@@ -45,12 +49,13 @@ export default class View {
             for ( let j = 1; j < cell.length; j++ ) 
                 this.offCtx.lineTo(cell[j].x, cell[j].y);
             this.offCtx.closePath();
-            let hue = (data.sites[i].x / this.offscreen.width * 360 + data.timestamp * 0.02) % 360;  console.log(hue);
-            this.gradient(this.offCtx, data.sites[i], hue);
-            //this.offCtx.fillStyle = `hsla(${hue},70%,40%,0.3)`;
+            let hue = (data.sites[i].x / this.offscreen.width * 360 + data.timestamp * 0.02) % 360;
+            //this.radialGradient(this.offCtx, data.sites[i], hue);
+            const alpha = this.shimmer(data.timestamp, i);
+            this.offCtx.fillStyle = `hsla(${hue},70%,40%,${alpha})`;
             this.offCtx.strokeStyle = `hsla(${hue},80%,70%,0.5)`;
             this.offCtx.fill();
-            this.offCtx.stroke();
+            //this.offCtx.stroke();
         }
         //this.overlay.draw(this.offCtx, this.offscreen.width, this.offscreen.height);
         this.blit();
