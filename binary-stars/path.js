53ad748b289cfb3472844763a91ccb518236d64d
andyblaze <andy@mediagraphic.co.uk>
2025-10-05 15:38:00 +0100

    binary-stars

---
+++ binary-stars/path.js
@@ -0,0 +1,19 @@
+export default function createBridgePath(donor, accretor) {
+    const start = { x: donor.pos.x, y: donor.pos.y };
+    const end = { x: accretor.pos.x, y: accretor.pos.y };
+
+    // Control point slightly offset toward the accretor to give a curve
+    const mid = {
+        x: (start.x + end.x) / 2,
+        y: (start.y + end.y) / 2 - 20  // adjust vertical offset for curvature
+    };
+
+    // Return a function that maps t ∈ [0,1] to a point on the quadratic Bezier curve
+    return function(t) {
+        const invT = 1 - t;
+        return {
+            x: invT * invT * start.x + 2 * invT * t * mid.x + t * t * end.x,
+            y: invT * invT * start.y + 2 * invT * t * mid.y + t * t * end.y
+        };
+    };
+}
--- binary-stars/view.js
+++ binary-stars/view.js
@@ -1,13 +1,25 @@
 import { getCanvas } from "./config.js";
+import createBridgePath from "./path.js";
 
 export default class View {
     constructor(config) {
+        this.cfg = config;
         this.canvas = getCanvas();
         this.ctx = this.canvas.getContext("2d");
     }
     draw(data) {
+        const path = createBridgePath(data[1], data[2]);
         for ( const d of data ) {
             d.draw(this.ctx);
+        }
+        // Example: draw 10 sample points along the path
+        for (let i = 0; i <= 10; i++) {
+            const t = i / 10;
+            const {x, y} = path(t);
+            const sx = this.canvas.width / 2 + x * this.cfg.visualScale;
+            const sy = this.canvas.height / 2 + y * this.cfg.visualScale;
+            this.ctx.fillStyle = "rgba(255,255,255,1)";
+            this.ctx.fillRect(sx, sy, 2, 2); // small visual placeholder
         }
     }
 }
