9c1da53fc7051c5206488d7d3869d40f52f6357a
andyblaze <andy@mediagraphic.co.uk>
2025-11-28 15:42:33 +0000

    feistel net

--- feistel/scheduler.js
+++ feistel/scheduler.js
@@ -8,23 +8,24 @@ const AnimationRegistry = {
 class AnimationFactory {
     constructor() {
         this.registry = {};
+        this.canvas = null;
     }
-    register(type, ctor) {
-        this.registry[type] = ctor;
+    init(c) {
+        this.canvas = c;
     }
-    create(canvas, type, data, config) {
-        const Ctor = this.registry[type];
-        if (!Ctor) throw new Error(`Unknown animation type: ${type}`);
-        return new Ctor(canvas, data, config);
+    create(type, data, config) {
+        const A = AnimationRegistry[type];
+        if ( ! A ) 
+            throw new Error(`Unknown animation type: ${type}`);
+        return new A(this.canvas, data, config);
     }
 }
 const animationFactory = new AnimationFactory();
-animationFactory.register("textSliderH", HorizontalTextSlider);
 
 const animationConfig = [
-  { t: 0,  fired:false, type: "textSliderH", "config": {"speed": -4, "y":80} }//,
-  /*{ t: 2,  type: "alphabetMap",  data: visitor.transformed_input },
-  { t: 5,  type: "splitBlock",   data: visitor.block_split },
+  { t: 0, fired:false, type: "textSliderH", "config": {"speed": -4, "y":80} },
+  { t: 1, fired:false, type: "textSliderH", "config": {"speed": 4, "y":40} }//,
+  /*{ t: 5,  type: "splitBlock",   data: visitor.block_split }/*,
   { t: 7,  type: "xorRound",     data: visitor.round0_xor },
   { t: 9,  type: "swap",         data: visitor.after_swap0 },
   { t: 12, type: "xorRound",     data: visitor.round1_xor },
@@ -35,17 +36,12 @@ export default class Scheduler {
     constructor(animator, events) {
         this.animator = animator;
         this.events = events;
-        //console.log(events);
-        this.steps = [
-            0
-        ];
-        this.running = [];
+        animationFactory.init(animator.canvas);
     }
     update(elapsedSeconds) {
         for ( let item of animationConfig ) {
             if ( false === item.fired && elapsedSeconds >= item.t) {
                 this.animator.add(animationFactory.create(
-                    this.animator.canvas,
                     item.type, 
                     this.events[item.t], 
                     item.config
