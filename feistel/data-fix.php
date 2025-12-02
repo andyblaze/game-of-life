81c0b8f70349c4c4057417aecf041143ddd37010
andyblaze <andy@mediagraphic.co.uk>
2025-12-02 10:27:55 +0000

    feistel

--- feistel/data-fix.php
+++ feistel/data-fix.php
@@ -1,8 +1,5 @@
 <?php 
 
-function replace($from, $to, $d) {
-    return str_replace($from, $to, $d);
-}
 $fname = 'view-data.json';
 $changes = [
     ['{"encrypt":[', "{\"encrypt\":[\n"],
@@ -14,24 +11,10 @@ $changes = [
     ['data":{"left', "data\":{\n\t\"left"],
     [',"right":', ",\n\t\"right\":"]
 ];
+
 $data = file_get_contents($fname);
 foreach ( $changes as $change) {
     list($from, $to) = $change;
     $data = str_replace($from, $to, $data);
 }
-
-/*$data = rep('{"encrypt":[', "{\"encrypt\":[\n", $data);
-$data = rep('{"decrypt":[', "{\"decrypt\":[\n", $data);
-
-$data = rep(',{"time', ",\n{\"time", $data);
-
-$data = rep('","data"', "\",\n\"data\"", $data);
-
-$data = rep('data":{"string', "data\":{\n\t\"string", $data);
-
-$data = rep('","array', "\",\n\t\"array", $data);
-
-$data = rep('data":{"left', "data\":{\n\t\"left", $data);
-$data = rep(',"right":', ",\n\t\"right\":", $data);*/
-
 file_put_contents($fname, $data);
