26bfe0fb1bc9e0e434f422bc330ecdb6e7a95b61
andyblaze <andy@mediagraphic.co.uk>
2025-12-07 14:49:29 +0000

    feistel

--- feistel/intro-mediator.js
+++ feistel/intro-mediator.js
@@ -2,14 +2,34 @@
 import TextSlider from "./textslider.js";
 import Animation from "./animation.js";
 import EventContext from "./event-context.js";
+import AnimationFactory from "./animation-factory.js";
+
+//const animationFactory = new AnimationFactory();
 
 export default class IntroMediator extends Animation {
     static type = "iMediator";
     constructor(cnvs) {
         super(cnvs);
-        this.plaintext = new TextSlider(cnvs, EventContext.byId("encrypt", "plaintext"), {"speed": -4, "y":20});
-        this.alphabet = new TextSlider(cnvs, EventContext.byId("encrypt", "alphabet"), {"speed": 1, "x": "mid", "endAt":60, "axis": "vertical"});
-        this.indices = new TextSlider(cnvs, EventContext.byId("encrypt", "indices"), {"speed": 1, "x": "mid", "endAt":88, "axis": "vertical"});
+        this.animationFactory = new AnimationFactory();
+        this.animationFactory.init(this.canvas);
+        this.plaintext = this.animationFactory.create(
+            TextSlider.type, 
+            EventContext.byId("encrypt", "plaintext"), 
+            {"speed": -4, "y":20}
+        );
+        this.alphabet = this.animationFactory.create(
+            TextSlider.type, 
+            EventContext.byId("encrypt", "alphabet"), 
+            {"speed": 1, "x": "mid", "endAt":60, "axis": "vertical"}
+        );
+        this.indices = this.animationFactory.create(
+            TextSlider.type, 
+            EventContext.byId("encrypt", "indices"), 
+            {"speed": 1, "x": "mid", "endAt":88, "axis": "vertical"}
+        );
+        //this.plaintext = new TextSlider(cnvs, EventContext.byId("encrypt", "plaintext"), {"speed": -4, "y":20});
+        //this.alphabet = new TextSlider(cnvs, EventContext.byId("encrypt", "alphabet"), {"speed": 1, "x": "mid", "endAt":60, "axis": "vertical"});
+        //this.indices = new TextSlider(cnvs, EventContext.byId("encrypt", "indices"), {"speed": 1, "x": "mid", "endAt":88, "axis": "vertical"});
     }
     isComplete() {
         return (this.plaintext.isComplete()
