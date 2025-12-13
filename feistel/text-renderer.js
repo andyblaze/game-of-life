9bbbc3b64fa80cce74f72e5c56387535ef7f0551
andyblaze <andy@mediagraphic.co.uk>
2025-12-13 10:33:26 +0000

    feistel

---
+++ feistel/text-renderer.js
@@ -0,0 +1,47 @@
+import Animation from "./animation.js";
+import EventContext from "./event-context.js";
+import LayoutRegistry from "./layout-registry.js";
+
+export default class TextRenderer extends Animation {
+    static type = "textRenderer";
+    constructor(cnvs, event, cfg) {
+        super(cnvs);
+        const evt = EventContext.byId(cfg.direction, cfg.type); 
+        this.event = evt;
+        this.tokens = evt.data.array;
+        this.msg = evt.data.string;
+        this.x = cfg.x; // default x
+        this.y = cfg.y; // default y
+        this.textSz = this.measureText(this.msg);
+        this.justify = cfg.justify ?? "left";
+        this.drawX = this.justifyText();
+    }
+    justifyText() {
+        if ( this.justify === "left" )      return this.x;
+        const rect = this.getBoundingRect();
+        if ( this.justify === "right" )     return this.x - rect.w;
+        if ( this.justify === "center" )    return (this.canvas.width - rect.w) / 2;
+        return this.x;
+    }
+    getBoundingRect(reMeasure=false) {
+        if ( true === reMeasure )
+            this.textSz = this.measureText(this.msg);
+        return {
+            x: this.x,
+            y: this.y,
+            w: this.textSz.width,
+            h: this.textSz.height + 2
+        };
+    }
+    draw() {
+        if ( this.msg.length === 0 ) return;
+        this.ctx.fillText(this.msg, this.drawX, this.y);
+        this.registerLayout();
+        this.animationDone = true;
+    }
+    clear() {
+        const rect = this.getBoundingRect();
+        this.ctx.clearRect(rect.x, rect.y, rect.w, rect.h);
+        this.msg = "";
+    }
+}
--- feistel/token-renderer.js
+++ feistel/token-renderer.js
@@ -1,27 +1,15 @@
 import Animation from "./animation.js";
 import EventContext from "./event-context.js";
 import LayoutRegistry from "./layout-registry.js";
+import TextRenderer from "./text-renderer.js";
 
-export default class TokenRenderer extends Animation {
+export default class TokenRenderer extends TextRenderer {
     static type = "tokenRenderer";
     constructor(cnvs, event, cfg) {
-        super(cnvs);
-        const evt = EventContext.byId(cfg.direction, cfg.type); 
-        this.event = evt;
-        this.tokens = evt.data.array;
+        cfg.justify = "center";
+        super(cnvs, event, cfg);
         this.currentIndex = 0;
         this.msg = ""; // accumulated text
-        this.x = cfg.x; // default x
-        this.y = cfg.y; // default y
-    }
-    getBoundingRect() {
-        const textSz = this.measureText(this.msg);
-        return {
-            x: this.x,
-            y:  this.y,
-            w: textSz.width,
-            h: textSz.height + 2
-        };
     }
     nextCharacter() {
         this.msg += this.tokens[this.currentIndex];
@@ -34,13 +22,8 @@ export default class TokenRenderer extends Animation {
     }
     draw() {
         if ( this.msg.length === 0 ) return;
-        const rect = this.getBoundingRect();
-        this.x = (this.canvas.width - rect.w) / 2;
-        this.ctx.fillText(this.msg, this.x, this.y);
-    }
-    clear() {
-        const rect = this.getBoundingRect();
-        this.ctx.clearRect(rect.x, rect.y, rect.w, rect.h);
-        this.msg = "";
+        this.getBoundingRect(true); // force re-measure because msg length may have changed
+        this.drawX = this.justifyText();
+        this.ctx.fillText(this.msg, this.drawX, this.y);
     }
 }
