fbaf0452256822aeacd2589c0d5105a82a9f74fc
andyblaze <andy@mediagraphic.co.uk>
2025-12-19 11:28:33 +0000

    wc

--- _wcm/js/main.js
+++ _wcm/js/main.js
@@ -11,6 +11,22 @@ ambientEvents.add(new SeaEvents($("#events-text")));
 
 $(window).on("resize", checkOrientation);
 
+$(document).on("click", ".town", function (e) {
+  const name = $(this).data("name");
+    const $popup = $("#town-popup");
+  $popup.text(name);
+
+  $popup.css({
+    left: e.pageX + 10,
+    top: e.pageY + 10
+  }).fadeIn(250);
+});
+$(document).on("click", function (e) {
+  if (!$(e.target).closest(".town").length) {
+   $("#town-popup").fadeOut(250);
+  }
+});
+
 let lastTime = 0;
 function animate(time) {
     const dt = (time - lastTime); // ms
