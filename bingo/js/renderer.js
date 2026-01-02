104de11e50ce4b74caa664325f99defe9711b449
andyblaze <andy@mediagraphic.co.uk>
2026-01-02 22:38:05 +0000

    bingo

--- bingo/js/renderer.js
+++ bingo/js/renderer.js
@@ -4,15 +4,20 @@ export default class Renderer {
     renderCard(card) {
         console.log(card);
         const target = $("#bingocard");
-        const grid = card.grid;
+        const grid = card.getGrid();
         let htm = "";
         let number = 0;
         for ( let y = 0; y < 5; y++ ) {
+            htm += `<div class="row">`;
             for ( let x = 0; x < 5; x++ ) {
                 number = grid[x][y].number;
-                htm += `<div id="card${x}-${y}"><span id="card${number}">${number}</span></div>`;
+                htm += `
+                <div class="cell" id="card${x}-${y}">
+                    <span id="card${number}">${number}</span>
+                </div>
+            `;
             }
-            htm += "<br>";
+            htm += "</div>";
         }
         target.html(htm);
     }
