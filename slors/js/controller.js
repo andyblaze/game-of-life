2e143879bbc263291a4253c05f8aa0ccfc90d7da
andyblaze <andy@mediagraphic.co.uk>
2026-01-06 22:34:54 +0000

    slots

--- slors/js/controller.js
+++ slors/js/controller.js
@@ -1,71 +1,45 @@
-import BingoCard from "./bingocard.js";
-import BingoScorer from "./bingo-scorer.js";
-import { config } from "./config.js";
-import GridGenerator from "./grid-generator.js";
-
-export default class BingoController {
-    constructor(engine, caller, renderer) {
+export default class SlotController {
+    constructor(engine, machine, ui) {
         this.engine = engine;
-        this.caller = caller;
-        this.renderer = renderer;
-        this.cards = [];
-        this.hasWon = false;
-        this.scorer = new BingoScorer(config.prizeMap); // default prizes
+        this.machine = machine;
+        this.ui = ui;
+
         this.bindEvents();
     }
-    setCardManager(cm) {
-        this.cardManager = cm;
-    }
+
     bindEvents() {
-        this.engine.on("state:enter:READY", () => {
-            this.onReady();
+        this.engine.on("state:enter:SPINNING", () => {
+            this.onSpinning();
         });
 
-        this.engine.on("state:enter:CHECKING", () => {
-            this.onDrawComplete();
+        this.engine.on("state:enter:EVALUATING", () => {
+            this.onEvaluating();
+        });
+
+        this.engine.on("state:enter:PAYOUT", () => {
+            this.onPayout();
         });
     }
-    onReady() {
-        if ( this.cards.length === 0 ) {  this.scorer.incoming += 18;
-            
-            this.hasWon = false;
-            //this.card = new BingoCard(config.gridSize, config.ranges, new GridGenerator(config.gridSize, config.ranges));
-            this.cards = this.cardManager.generateMultipleCards(config.gridSize, config.ranges, 18);
-            //console.log(this.card);
-            this.renderer.renderCards(this.cards);
-        }
-    }
-    onDrawComplete() {
-        const number = this.caller.getLastNumber();
-        const text = this.caller.getLastCall();
-        this.renderer.displayCall(text);
-        const winners = [];
 
-        for ( const [idx, card] of this.cards.entries() ) {
-            card.mark(number);
-            this.renderer.markCards(number);
-
-            const scores = this.scorer.calculate(card);
-            if ( scores.length ) { //console.log(scores);
-                winners.push({ index: idx, "card": card });
-            }
-        }
-        if ( winners.length > 0 ) {
-                this.renderer.markWinningLines(winners); 
-                this.renderer.showProfit(this.scorer.getProfit());
-                this.hasWon = true;
-                console.log("🎉 BINGO!");
-                console.log("Total score: ", this.scorer.totalScore());
-                this.engine.dispatch("END_GAME");
-                return;
-            //}
-        }
-       this.engine.dispatch("CHECK_COMPLETE");
+    onSpinning() {
+        const result = this.machine.spin();
+        this.ui.animateSpin(result, () => {
+            this.engine.dispatch("SPIN_COMPLETE", result);
+        });
     }
+
+    onEvaluating() {
+        this.engine.dispatch("EVALUATE_COMPLETE");
+    }
+
+    onPayout() {
+        this.ui.animatePayout(() => {
+            this.engine.dispatch("PAYOUT_COMPLETE");
+        });
+    }
+
     reset() {
-        this.cards = [];
-        this.cardManager.reset();
-        this.scorer.reset();
+        this.machine.reset?.();
+        this.ui.reset?.();
     }
 }
-
