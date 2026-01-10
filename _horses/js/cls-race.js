180940cef809b5012b6216e91add3faac84be307
andyblaze <andy@mediagraphic.co.uk>
2026-01-10 15:30:19 +0000

    horses

--- _horses/js/cls-race.js
+++ _horses/js/cls-race.js
@@ -15,18 +15,12 @@ export default class Race {
   run() {
     // Use RaceResolver to compute results
    /// this.results = RaceResolver.resolve(this.track, this.distance, this.entrants, this.trainers);
-    const form = {
+    const result = {
         trackId: this.track.id,
         raceId: this.id,
         distance: this.distance,
         results: RaceResolver.resolve(this.track, this.distance, this.entrants, this.trainers)
       };
-    /*for ( const[index, result] of this.results.entries() ) //.forEach((horse, index) => {      
-        form.results.push({
-          horseId: result.horse.id,
-          trainerId: result.horse.trainerId,
-          position: index + 1,
-      });*/
-    return form;//this.results;
+    return result;
   }
 }
