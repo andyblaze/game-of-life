5cbc7593182d5c1d1418a203ed50ed59783b5f1b
andyblaze <andy@mediagraphic.co.uk>
2026-01-16 14:25:27 +0000

    weather

--- weather/js/observers.js
+++ weather/js/observers.js
@@ -5,10 +5,26 @@ export class ConsoleObserver {
     }
 }
 
+class HtmlRenderer {
+    output(at, data, prefix="", suffix="", css=null) {
+        const htm = prefix + data + suffix;
+        $(at).html(htm);
+        if ( css !== null )
+            $(at).css(css.key, css.var);
+    }
+    drawTemp(data) {
+        this.output("#temp", data, "", "&deg; C");
+    }
+    drawWindArrow(css) {
+        $("#wind-arrow").css(css.key, css.var);
+    }
+}
+
 export class Simpleton { 
     constructor(c) {
         this.container = c; //console.log(this.container);
         this.state = {};
+        this.renderer = new HtmlRenderer();
     }
     update(readings) { 
         for ( const type in readings )
@@ -18,11 +34,13 @@ export class Simpleton {
         this.render();
     }
     render() { 
-        this.container.find("#temp").html(this.state["temp"].C + "&deg; C");
+        this.renderer.drawTemp(this.state["temp"].C);
+        //this.container.find("#temp").html(this.state["temp"].C + "&deg; C");
         this.container.find("#wind-speed").html(this.state["wind"].MPH + " MPH");
         this.container.find("#wind-dir").html(this.state["wind_dir"].DEG + "&deg;");
         this.container.find("#cloud").html(this.state["cloud"].PC + "%");
         this.container.find("#rain").html(this.state["rain"].INH + " Inches per hour");
-        $("#wind-arrow").css("transform",`rotate(${this.state["wind_dir"].DEG}deg)`);
+        this.renderer.drawWindArrow({ key: "transform", var: `rotate(${this.state["wind_dir"].DEG}deg)` });
+        //.css(,);
     }        
 }
