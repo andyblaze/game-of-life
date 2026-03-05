1df3626b5a98a24a22f8692c9bc6048e058eaaa5
andyblaze <andy@mediagraphic.co.uk>
2026-03-05 15:27:42 +0000

    spiro

--- spyrograrf/js/config.js
+++ spyrograrf/js/config.js
@@ -1,21 +1,31 @@
 import { byId } from "./functions.js";
 
 export default class Config {
-    constructor(canvasId, workspaceId) {
+    constructor(canvasId, workspaceId, tc) {
         this.canvas = byId(canvasId);
         this.ctx = this.canvas.getContext("2d");
-        const workspace = byId(workspaceId);
-        const rect = workspace.getBoundingClientRect();
+        const rect = byId(workspaceId).getBoundingClientRect();
         this.canvas.width = rect.width;
         this.canvas.height = window.innerHeight
         this.canvasW = this.canvas.width;
         this.canvasH = this.canvas.height;
         this.centerX = this.canvas.width / 2;
         this.centerY = this.canvas.height / 2;
-        this.outerRadius = 150;
-        this.innerRadius = 35;
-        this.penOffset = 40;
+        this.outerRadius = 251;
+        this.innerRadius = 109;
+        this.penOffset = 91;
         this.theta = 0;
-        this.speed = 0.1;
+        this.speed = 0.2;
+    }
+    updateCtrl(ctrl) { 
+        const type = ctrl.dataset.type;
+        const property = ctrl.dataset.property;
+        this[property] = this.converter.apply(type, ctrl, ctrl.value);
+        this.controlsData[property] = this.converter.apply(type, ctrl, ctrl.value);
+    }
+    update(ctrls) {
+        for ( const ctrl of ctrls ) {
+            this.updateCtrl(ctrl);
+        }
     }
 }
