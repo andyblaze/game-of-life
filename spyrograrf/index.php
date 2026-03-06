5f35473e9c9624ad101b0c63b0accadd707593ad
andyblaze <andy@mediagraphic.co.uk>
2026-03-06 11:09:34 +0000

    spiro

--- spyrograrf/index.php
+++ spyrograrf/index.php
@@ -77,6 +77,7 @@ function render($view, $data = []) {
 $viewData = [
     'outerRadiusXCtrl' => slider('outerRadiusX', 20, 360, 1, 140, 'int', 'outerRadiusX'),
     'outerRadiusYCtrl' => slider('outerRadiusY', 20, 360, 1, 140, 'int', 'outerRadiusY'),
+    'rotationCtrl'     => slider('rotation', 0, 359, 1, 0, 'int', 'rotation'),
     'innerRadiusCtrl' => slider('innerRadius', 20, 140, 1, 60, 'int', 'innerRadius'),
     'penOffsetCtrl'   => slider('penOffest', 2, 30, 1, 15, 'int', 'penOffset'),
     'speedCtrl'       => slider('speed', 0.01, 0.5, 0.01, 0.2, 'float', 'speed')
--- spyrograrf/js/core.js
+++ spyrograrf/js/core.js
@@ -21,7 +21,29 @@ export default class Core {
         this.t += dt;
     }
     getPoint(t) {
-        const baseX = (this.Rx - this.r) * Math.cos(this.t);
+    // Base ellipse point
+    const ex = (this.Rx - this.r) * Math.cos(this.t);
+    const ey = (this.Ry - this.r) * Math.sin(this.t);
+
+    // Rotate the ellipse by angleRad
+    const baseX = ex * Math.cos(this.angleRad) - ey * Math.sin(this.angleRad);
+    const baseY = ex * Math.sin(this.angleRad) + ey * Math.cos(this.angleRad);
+
+    // Pen rotation around rolling circle
+    const rot = ((this.Rx - this.r) / this.r) * this.t;
+    const penX = this.d * Math.cos(rot);
+    const penY = this.d * Math.sin(rot);
+
+    // Combine base ellipse and pen offset
+    const x = baseX + penX;
+    const y = baseY - penY;
+
+    // Translate to canvas center
+    return {
+        x: this.cfg.centerX + x,
+        y: this.cfg.centerY + y
+    };
+        /*const baseX = (this.Rx - this.r) * Math.cos(this.t);
         const baseY = (this.Ry - this.r) * Math.sin(this.t);
 
         const rot = ((this.Rx - this.r) / this.r) * this.t;
@@ -35,6 +57,6 @@ export default class Core {
         return {
             x: this.cfg.centerX + x,
             y: this.cfg.centerY + y
-        };
+        };*/
     }
 }
