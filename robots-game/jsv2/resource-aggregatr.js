716bfe734642db59a3aea332f2377eeca6926298
andyblaze <andy@mediagraphic.co.uk>
2026-04-04 14:13:24 +0100

    robotgame

--- robots-game/jsv2/resource-aggregatr.js
+++ robots-game/jsv2/resource-aggregatr.js
@@ -9,9 +9,12 @@ export default class ResourceAggregator extends GameItem {
         this.resources.push(resource);
     }
     produce(world) {
+        console.log("pre", this.result);
         for ( const r of this.resources ) {
             this.result += r.tick(world).amount;
+            console.log(this.product, "in", this.result);
         }
+        console.log("post", this.result);
     }
     finalise(world) {
         world.deposit({ type: this.product, amount: this.result });
